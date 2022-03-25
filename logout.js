84efe05d4f609a06baa4b1032017affc592b8084
gilbertUp <giulzitu@gmail.com>
2022-03-25 13:47:58 +0200

    add up

--- logout.js
+++ logout.js
@@ -1,14 +1,14 @@
-function logout(){
-    // let users = JSON.parse(localStorage.getItem('users'));
-    // if(username==0){
-    //     users.loggedout=true
-    //     localStorage.setItem("users", JSON.stringify(users));
-    //     alert("logout successfully")
-    // }
-    // else {
-    //     window.location.href='./loginpage.html'
-    // }
-    localStorage.removeItem('token')
-    localStorage.removeItem('role')
-    window.location.href = '../login.html'
-  }
+// function logout(){
+//     // let users = JSON.parse(localStorage.getItem('users'));
+//     // if(username==0){
+//     //     users.loggedout=true
+//     //     localStorage.setItem("users", JSON.stringify(users));
+//     //     alert("logout successfully")
+//     // }
+//     // else {
+//     //     window.location.href='./loginpage.html'
+//     // }
+//     localStorage.removeItem('token')
+//     localStorage.removeItem('role')
+//     window.location.href = '../login.html'
+//   }
